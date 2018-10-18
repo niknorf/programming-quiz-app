399e8a1954e5a3a32a8584db40ab26582d371f62
Nikita Volynkin <t5voni00@students.oamk.fi>
2018-10-18 11:22:59 +0300

    Quiz app
    Quiz app with Main, Quiz Game, Add Questions and High Scores pages and rooting.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './table/table.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
+import { QuestionsPageComponent } from './questions-page/questions-page.component';
+import { ScoresPageComponent } from './scores-page/scores-page.component';
+
+const appRoutes: Routes = [
+  { path: 'main-page', component: MainPageComponent },
+  { path: 'game-page', component: GamePageComponent },
+  { path: 'questions-page', component: QuestionsPageComponent },
+  { path: 'scores-page', component: ScoresPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    TableComponent,
+    MainPageComponent,
+    GamePageComponent,
+    QuestionsPageComponent,
+    ScoresPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/scores-page/scores-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scores-page',
+  templateUrl: './scores-page.component.html',
+  styleUrls: ['./scores-page.component.scss']
+})
+export class ScoresPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
