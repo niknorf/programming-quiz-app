a96573e4a5bd5bacd82167a353f3c17dd82f2164
Nikita Volynkin <t5voni00@students.oamk.fi>
2018-10-19 01:58:39 +0300

    Added routing to the game page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -70,6 +72,7 @@ const appRoutes: Routes = [
     MatPaginatorModule,
     MatSortModule,
     MatInputModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [ElementHelperService],
